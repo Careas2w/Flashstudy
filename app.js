606611bb2b249fefab2b5668b6b0b1b47dfbcd24
Careas2w <careaspartulepe@gmail.com>
2026-04-05 20:20:14 +0200

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./sw.js').catch(err => {
-        console.warn('No se pudo registrar el service worker:', err);
-      });
-    });
-  }
